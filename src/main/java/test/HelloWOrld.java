c130844d1643ca49e30fd66cc3b2d11c982eff2a
Ajay Ramesh <ajaycs14@gmail.com>
2017-01-27 13:28:57 -0600

    Init Hello World

---
+++ src/main/java/test/HelloWOrld.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * Created by Ajay on 1/27/17.
+ */
+public class HelloWOrld {
+    public static void main(String[] args) {
+        System.out.print("Hello World");
+    }
+}
