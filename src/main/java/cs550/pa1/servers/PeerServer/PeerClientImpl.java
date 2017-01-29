d6509d0aa99c737133edd9d91cf27e584dd3c744
Ajay Ramesh <ajaycs14@gmail.com>
2017-01-28 19:43:56 -0600

    Final Submission PA1 : Init

--- src/main/java/cs550/pa1/servers/PeerServer/PeerClientImpl.java
+++ src/main/java/cs550/pa1/servers/PeerServer/PeerClientImpl.java
@@ -37,7 +37,7 @@ public class PeerClientImpl implements Peer {
     }
 
 
-    public void peerClientInterface() throws  IOException{
+    public void peerClientInterface() {
 
         String fileName="";
         try {
