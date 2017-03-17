ed8bf899c7cf605f8b4b844503b82b30347289f2
Ajay Ramesh <ajaycs14@gmail.com>
2017-03-16 21:03:46 -0500

    Junit Test cases Update

---
+++ src/main/java/cs550/pa3/test/ApplicationRunnerTest.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2017.  FileSharingSystem - https://github.com/ajayramesh23/FileSharingSystem
+ * Programming Assignment from Professor Z.Lan
+ * @author Ajay Ramesh
+ * @author Chandra Kumar Basavaraj
+ * Last Modified - 3/16/17 2:22 PM
+ */
+
+package cs550.pa3.test;
+
+import cs550.pa3.ApplicationRunner;
+import org.junit.After;
+import org.junit.Test;
+
+import java.util.Scanner;
+
+public class ApplicationRunnerTest {
+    ApplicationRunner runner;
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void main() throws Exception {
+        Scanner scanner = new Scanner("y");
+/*
+        assertEquals(0, programContinueSelect(scanner));
+        runner = new ApplicationRunner(scanner);
+*/
+
+
+    }
+
+}
